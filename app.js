bfb9b20118aa547d25ecf6a6ba1e45868d4c1ae7
JoeTerol <teroljoseeduardo@gmail.com>
2023-06-16 19:04:17 -0300

    rename the index.html/ main.html

--- app.js
+++ app.js
@@ -9,4 +9,10 @@ const fs = require('fs');
 //     console.log(contenido);
   
 // });
+fs.rename('index.html','main.html', (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Nombre Cambiado Exitosamente')
+});
 
