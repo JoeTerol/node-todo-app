2c150c05a258d3e9a0cc1ac588f7ed992ab727b2
JoeTerol <teroljoseeduardo@gmail.com>
2023-06-16 18:55:28 -0300

    new fs.readFile

---
+++ app.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+
+// Leer Archivos
+// fs.readFile('inde.html', 'utf-8', (err, contenido) => {
+//   if (err){
+//     throw err;
+//   } 
+//     console.log(contenido);
+  
+// });
+
