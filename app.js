9d974808bd791df927365492c5c569a365e0759f
JoeTerol <teroljoseeduardo@gmail.com>
2023-06-17 13:40:24 -0300

    methods sync

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
 const fs = require('fs');
 
-
+console.log('antes de leer un archivo');
 // Leer Archivos
-// fs.readFile('inde.html', 'utf-8', (err, contenido) => {
-//   if (err){
-//     throw err;
-//   } 
-//     console.log(contenido);
-// });
-
+fs.readFile('inde.html', 'utf-8', (err, contenido) => {
+  if (err){
+    throw err;
+  } 
+    console.log(contenido);
+});
+console.log('despues de leer un archivo');
 
 // Renombrar Archivos
-// fs.rename('index.html','main.html', (err) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log('Nombre Cambiado Exitosamente')
-// });
+fs.rename('index.html','main.html', (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Nombre Cambiado Exitosamente')
+});
+console.log('despues de renombrar un archivo');
 
 
 // Agregar contenido al final del archivo, este metodo crea un archivo nuevo si este no existe
-// fs.appendFile('index.html', '<p>Hola</p>', (err) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log ('Archivo Actualizado')
-// });
+fs.appendFile('index.html', '<p>Hola</p>', (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log ('Archivo Actualizado')
+});
+console.log('despues de actualizar un archivo');
 
 
 // Reemplazar el contenido del archivo.
-// fs.writeFile('index.html', 'contenido Nuevo ', (err) => {
-//   if (err) {
-//     throw err;
-//   } console.log('Contenido Reemplazado Exitosamente')
-// });
-
+fs.writeFile('index.html', 'contenido Nuevo ', (err) => {
+  if (err) {
+    throw err;
+  } console.log('Contenido Reemplazado Exitosamente')
+});
+console.log('despues de reemplazar a un archivo');
 
 // Eliminar un archivo
 
@@ -46,4 +48,4 @@ fs.unlink('main.html', (err) => {
  }
  console.log('archivo eliminado.');
 });
-
+console.log('despues de eliminar un archivo');
