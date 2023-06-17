39ea84c6896080cb75038c4ab641cd1dbae7b844
JoeTerol <teroljoseeduardo@gmail.com>
2023-06-16 23:35:57 -0300

    write file

--- app.js
+++ app.js
@@ -7,12 +7,31 @@ const fs = require('fs');
 //     throw err;
 //   } 
 //     console.log(contenido);
-  
 // });
-fs.rename('index.html','main.html', (err) => {
+
+
+// Renombrar Archivos
+// fs.rename('index.html','main.html', (err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log('Nombre Cambiado Exitosamente')
+// });
+
+
+// Agregar contenido al final del archivo, este metodo crea un archivo nuevo si este no existe
+// fs.appendFile('index.html', '<p>Hola</p>', (err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log ('Archivo Actualizado')
+// });
+
+// Reemplazar el contenido del archivo.
+
+fs.writeFile('index.html', 'contenido Nuevo ', (err) => {
   if (err) {
     throw err;
-  }
-  console.log('Nombre Cambiado Exitosamente')
+  } console.log('Contenido Reemplazado Exitosamente')
 });
 
