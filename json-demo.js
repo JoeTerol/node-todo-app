e74605c6979c72886c6a046f6cb3e81e91bae859
JoeTerol <teroljoseeduardo@gmail.com>
2023-06-18 22:22:34 -0300

    parse & stringfy JSON

--- json-demo.js
+++ json-demo.js
@@ -1,3 +1,22 @@
-const curso = require('./curso.json');
+let infoCurso = {
+    "titulo": "Aprende Node.js",
+    "numVisitas": 45642,
+    "numLikes": 21123,
+    "temas": [
+         "JavaScript",
+         "Node.js"
 
-console.log( typeof curso)
+    ],
+    "esPubluico": true
+};
+console.log(infoCurso);
+// CONVERTIUR UN OBJETO EN STRING
+let infoCursoJSON = JSON.stringify(infoCurso);
+
+console.log(infoCursoJSON );
+console.log(typeof infoCursoJSON); 
+// COVNERTIR UN STRING EN UN OBJETO
+let infoCursoObjeto = JSON.parse(infoCursoJSON);
+
+console.log(infoCursoObjeto);
+console.log(typeof infoCursoObjeto);
